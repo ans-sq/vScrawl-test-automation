420fea92ad8eddbf59924a15188461fed012f3ec
Ans Sohail <anassohail101@gmail.com>
2023-05-29 18:51:23 +0500

    update

--- deleteFiles.js
+++ deleteFiles.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const filesToDelete = [
   'cypress\\results\\mochawesome_001.json',
   'cypress\\results\\mochawesome.json',
-  'mochawesome.json'
+  'Report-Result.json'
   // Add more files to delete if needed
 ];
 
