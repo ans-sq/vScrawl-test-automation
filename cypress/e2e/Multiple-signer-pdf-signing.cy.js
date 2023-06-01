dd32069669fde73aff0ccb5663aa64cb92432f9e
Ans Sohail <anassohail101@gmail.com>
2023-06-01 14:11:54 +0500

    m

--- cypress/e2e/Multiple-signer-pdf-signing.cy.js
+++ cypress/e2e/Multiple-signer-pdf-signing.cy.js
@@ -12,128 +12,124 @@ describe('Multiple signer pdf signing', () =>{
         cy.visit(URL)
 
         // enter email for first signer
-        cy.get('#email',{ timeout : 15000 }).type(email1)
+        cy.get('#email',{ timeout : 5000 }).type(email1)
   
         // enter password for first signer
-        cy.get('#password',{ timeout : 15000 }).type(password1)
+        cy.get('#password',{ timeout : 5000 }).type(password1)
   
-        cy.get('.fuse-mat-button-large',{ timeout : 15000 }).click()
+        cy.get('.fuse-mat-button-large',{ timeout : 5000 }).click()
   
         // select to upload a new document
-        cy.get('[routerlink="/documents/new"] > .flex-auto',{ timeout : 15000 }).click()
+        cy.get('[routerlink="/documents/new"] > .flex-auto',{ timeout : 5000 }).click()
 
-        cy.get('.text-base > .mat-focus-indicator > .mat-button-wrapper',{ timeout : 15000 }).click({ force: true })
+        cy.get('.text-base > .mat-focus-indicator > .mat-button-wrapper',{ timeout : 5000 }).click({ force: true })
 
         // upload the file to be signed
-        cy.get('input[type="file"]',{ timeout : 15000 }).selectFile('TestingDoc.pdf',{force:true})
+        cy.get('input[type="file"]',{ timeout : 5000 }).selectFile('TestingDoc.pdf',{force:true})
 
         // unselect checkbox for single signer
-        cy.get('.mat-checkbox-inner-container',{ timeout : 15000 }).click()
+        cy.get('.mat-checkbox-inner-container',{ timeout : 5000 }).click()
 
-        cy.get('.mat-primary',{ timeout : 15000 }).click()
+        cy.get('.mat-primary',{ timeout : 5000 }).click()
 
         // secondary signer name
-        cy.get('#mat-input-3',{ timeout : 15000 }).type('Signer')
+        cy.get('#mat-input-3',{ timeout : 5000 }).type('Signer')
 
         // secondary signer email
-        cy.get('#mat-input-4',{ timeout : 15000 }).type(email2)
+        cy.get('#mat-input-4',{ timeout : 5000 }).type(email2)
 
-        cy.get('.items-center > .mat-primary',{ timeout : 15000 }).click()
+        cy.get('.items-center > .mat-primary',{ timeout : 5000 }).click()
 
         //dragging and dropping a signature box on the pdf
-        cy.get('#menu_signature',{ timeout : 15000 }).drag('#pageNo_1 > .page', {
+        cy.get('#menu_signature',{ timeout : 5000 }).drag('#pageNo_1 > .page', {
             //descibes the target location on the pdf where it is being dropped
             target: { x:200,y:290 }
         })
 
         //dynamically getting the attribute for the signature box locator
-        cy.get('[id^="sign_"]',{ timeout : 15000 }).click()
+        cy.get('[id^="sign_"]',{ timeout : 5000 }).click()
 
-        cy.get('#mat-select-value-3',{ timeout : 15000 }).click()
+        cy.get('#mat-select-value-3',{ timeout : 5000 }).click()
 
-        cy.get('#mat-option-5 > .mat-option-text',{ timeout : 15000 }).click()
+        cy.get('#mat-option-5 > .mat-option-text',{ timeout : 5000 }).click()
 
         //dragging and dropping a signature box on the pdf
-        cy.get('#menu_signature',{ timeout : 15000 }).drag('#pageNo_1 > .page', {
+        cy.get('#menu_signature',{ timeout : 5000 }).drag('#pageNo_1 > .page', {
             //descibes the target location on the pdf where it is being dropped
             target: { x:350,y:290 }
         })
 
-        cy.get('#mat-select-value-3',{ timeout : 15000 }).click()
+        cy.get('#mat-select-value-3',{ timeout : 5000 }).click()
 
-        cy.get('#mat-option-4 > .mat-option-text',{ timeout : 15000 }).click()
+        cy.get('#mat-option-4 > .mat-option-text',{ timeout : 5000 }).click()
 
         //dragging and dropping a text box on the pdf
-        cy.get('#menu_text',{ timeout : 15000 }).drag('#pageNo_1 > .page', {
+        cy.get('#menu_text',{ timeout : 5000 }).drag('#pageNo_1 > .page', {
             //descibes the target location on the pdf where it is being dropped
             target: { x:200,y:150 }
         })
 
-        cy.get('input',{ timeout : 15000 }).clear()
+        cy.get('input',{ timeout : 5000 }).clear()
 
-        cy.get('input',{ timeout : 15000 }).type('Signer 1')
+        cy.get('input',{ timeout : 5000 }).type('Signer 1')
 
-        cy.get('#mat-select-value-3',{ timeout : 15000 }).click()
+        cy.get('#mat-select-value-3',{ timeout : 5000 }).click()
 
-        cy.get('#mat-option-5 > .mat-option-text',{ timeout : 15000 }).click()
+        cy.get('#mat-option-5 > .mat-option-text',{ timeout : 5000 }).click()
 
         //dragging and dropping a text box on the pdf
-        cy.get('#menu_text',{ timeout : 15000 }).drag('#pageNo_1 > .page', {
+        cy.get('#menu_text',{ timeout : 5000 }).drag('#pageNo_1 > .page', {
             //descibes the target location on the pdf where it is being dropped
             target: { x:350,y:150 }
         })
 
         // dynamically getting the locator of the first signature box to apply the signature
-        cy.get('[id^="sign_"]',{ timeout : 15000 }).first().click()
+        cy.get('[id^="sign_"]',{ timeout : 5000 }).first().click()
 
-        cy.get('.pl-2 > :nth-child(4) > div > .mat-focus-indicator',{ timeout : 15000 }).click()
+        cy.get('.pl-2 > :nth-child(4) > div > .mat-focus-indicator',{ timeout : 5000 }).click()
 
-        cy.get('[class^="mat-focus-indicator mat-stroked-button mat-button-base ng-star-inserted"]',{ timeout : 15000 }).click()
+        cy.get('[class^="mat-focus-indicator mat-stroked-button mat-button-base ng-star-inserted"]',{ timeout : 5000 }).click()
 
-        cy.get('.items-center > :nth-child(2) > .mat-focus-indicator',{ timeout : 15000 }).click()
+        cy.get('.items-center > :nth-child(2) > .mat-focus-indicator',{ timeout : 5000 }).click()
 
-        cy.get('.mat-stroked-button',{ timeout : 15000 }).click()
+        cy.get('.mat-stroked-button',{ timeout : 5000 }).click()
 
-        cy.get('.flex-0 > .mt-1',{ timeout : 15000 }).click()
+        cy.get('.flex-0 > .mt-1',{ timeout : 5000 }).click()
 
         // logging out first signer
-        cy.get('user > .mat-focus-indicator',{ timeout : 15000 }).click()
+        cy.get('user > .mat-focus-indicator',{ timeout : 5000 }).click()
 
-        cy.get(':nth-child(6) > span',{ timeout : 15000 }).click()
+        cy.get(':nth-child(6) > span',{ timeout : 5000 }).click()
 
         cy.wait(500)
 
         cy.visit(URL)
 
         // enter email for second signer
-        cy.get('#email',{ timeout : 15000 }).type(email2)
+        cy.get('#email',{ timeout : 5000 }).type(email2)
   
         // enter password for second signer
-        cy.get('#password',{ timeout : 15000 }).type(password2)
+        cy.get('#password',{ timeout : 5000 }).type(password2)
   
-        cy.get('.fuse-mat-button-large',{ timeout : 15000 }).click()
+        cy.get('.fuse-mat-button-large',{ timeout : 5000 }).click()
 
-        cy.get('.mat-paginator-navigation-last',{ timeout : 15000 }).then(($button) =>{
-            if($button.hasClass('active')){
-                $button.click()
-            }
-        })
+        cy.get('.mat-paginator-navigation-last',{ timeout : 5000 }).click()
 
         // dynamically getting the locator for the link to the last document to be signed 
-        cy.get('[role^="row"] > .cdk-column-name > a',{ timeout : 15000 }).last().click()
+        cy.get('[role^="row"] > .cdk-column-name > a',{ timeout : 5000 }).last().click()
 
-        cy.get(':nth-child(2) > .flex > div > .mat-focus-indicator',{ timeout : 15000 }).click()
+        cy.get(':nth-child(2) > .flex > div > .mat-focus-indicator',{ timeout : 5000 }).click()
 
-        cy.get('input',{ timeout : 15000 }).clear()
+        cy.get('input',{ timeout : 5000 }).clear()
 
         // signing the document through second signer
-        cy.get('input',{ timeout : 15000 }).type('Signer 2')
+        cy.get('input',{ timeout : 5000 }).type('Signer 2')
         
-        cy.get('[id^="sign_"]',{ timeout : 15000 }).click()
+        cy.get('[id^="sign_"]',{ timeout : 5000 }).click()
 
-        cy.get('.items-center > :nth-child(2) > .mat-focus-indicator',{ timeout : 15000 }).click()
+        cy.get('.items-center > :nth-child(2) > .mat-focus-indicator',{ timeout : 5000 }).click()
 
-        cy.get('.mat-flat-button',{ timeout : 15000 }).click()
+        cy.get('.mat-flat-button',{ timeout : 5000 }).click()
         
         // end of signing
 
--- cypress/e2e/Single-signer-pdf-signing.cy.js
+++ cypress/e2e/Single-signer-pdf-signing.cy.js
@@ -7,62 +7,60 @@ describe('Single signer pdf signing', () => {
     cy.visit(URL)
 
     //enter email
-    cy.get('#email',{ timeout : 15000 }).type(email)
+    cy.get('#email',{ timeout : 5000 }).type(email)
 
     //enter password
-    cy.get('#password',{ timeout : 15000 }).type(password)
+    cy.get('#password',{ timeout : 5000 }).type(password)
 
-    cy.get('.fuse-mat-button-large',{ timeout : 15000 }).click()
+    cy.get('.fuse-mat-button-large',{ timeout : 5000 }).click()
 
     //select to upload a new document
-    cy.get('[routerlink="/documents/new"] > .flex-auto',{ timeout : 15000 }).click()
+    cy.get('[routerlink="/documents/new"] > .flex-auto',{ timeout : 5000 }).click()
 
-    cy.get('.text-base > .mat-focus-indicator > .mat-button-wrapper',{ timeout : 15000 }).click({ force: true })
+    cy.get('.text-base > .mat-focus-indicator > .mat-button-wrapper',{ timeout : 5000 }).click({ force: true })
  
     //upload the file to be signed
-    cy.get('input[type="file"]',{ timeout : 15000 }).selectFile('TestingDoc.pdf',{force:true})
+    cy.get('input[type="file"]',{ timeout : 5000 }).selectFile('TestingDoc.pdf',{force:true})
 
     cy.wait(2000)
 
     cy.get('.mat-primary').click()
 
     //dragging and dropping a signature box on the pdf
-    cy.get('#menu_signature',{ timeout : 15000 }).drag('#pageNo_1 > .page', {
+    cy.get('#menu_signature',{ timeout : 5000 }).drag('#pageNo_1 > .page', {
       //descibes the target location on the pdf where it is being dropped
       target: { x:250,y:290 }
     })
 
     //dynamically getting the attribute for the signature box locator
-    cy.get('[id^="sign_"]',{ timeout : 15000 }).click()
+    cy.get('[id^="sign_"]',{ timeout : 5000 }).click()
 
-    cy.get('[id^="1"]',{ timeout : 15000 }).then(($object) => {
+    cy.get('[id^="1"]',{ timeout : 5000 }).then(($object) => {
           // Resize the object
           $object.css('width', '300px');
           $object.css('height', '200px');
       });
 
      //dragging and dropping a text box on the pdf
-     cy.get('#menu_text',{ timeout : 15000 }).drag('#pageNo_1 > .page', {
+     cy.get('#menu_text',{ timeout : 5000 }).drag('#pageNo_1 > .page', {
       //descibes the target location on the pdf where it is being dropped
       target: { x:200,y:150 }
     })
 
-    cy.get('input',{ timeout : 15000 }).clear()
+    cy.get('input',{ timeout : 5000 }).clear()
 
-    cy.get('input',{ timeout : 15000 }).type('Signer')
+    cy.get('input',{ timeout : 5000 }).type('Signer')
 
-    cy.get('[id^="1"]',{ timeout : 15000 }).then(($object) => {
+    cy.get('[id^="1"]',{ timeout : 5000 }).then(($object) => {
       // Resize the object
       $object.css('width', '200px');
   });
 
-    cy.wait(5000)
-
     //finishing the document
-    cy.get('.pl-2 > :nth-child(2) > .mat-focus-indicator',{ timeout : 15000 }).click()
+    cy.get('.pl-2 > :nth-child(2) > .mat-focus-indicator',{ timeout : 5000 }).click()
 
-    cy.get('.mat-stroked-button',{ timeout : 15000 }).click()
+    cy.get('.mat-stroked-button',{ timeout : 5000 }).click()
 
-    cy.get(':nth-child(3) > .mat-focus-indicator',{ timeout : 15000 }).click()
+    cy.get(':nth-child(3) > .mat-focus-indicator',{ timeout : 5000 }).click()
   })
 })
