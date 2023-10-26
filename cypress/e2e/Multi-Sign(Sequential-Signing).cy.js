a612b78646428025dfb46a1982692a64de725be0
Ans Sohail <anassohail101@gmail.com>
2023-10-26 18:22:03 +0500

    fix

--- cypress/e2e/Multi-Sign(Sequential-Signing).cy.js
+++ cypress/e2e/Multi-Sign(Sequential-Signing).cy.js
@@ -88,6 +88,7 @@ describe("Check Multi signing", () => {
        })
 
     //    // dynamically getting the locator of the first signature box to apply the signature
+    
     //    cy.get('[id^="sign_"]',{ timeout : 20000, retryInterval: 3000 }).first().click()
 
        cy.get('.pl-2 > :nth-child(4) > div > .mat-focus-indicator',{ timeout : 20000, retryInterval: 3000 }).click()
